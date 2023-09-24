7f8bad97781b7f62b5db13847918e12db77577a0
DearNoodle <wan619395@gmail.com>
2023-09-24 13:22:08 +0800

    add Game Rounds/Scores/RestartRound + changes to Result Message

--- script.js
+++ script.js
@@ -2,41 +2,67 @@ function getComputerChoice() {
     return CHOICE[Math.floor(Math.random()*CHOICE.length)];
 }
 
-function getPlayerChoice(str) {
-    return str.toLowerCase();
+function getPlayerChoice(input) {
+    return input.toLowerCase();
 }
 
 function playRound(player,computer) {
     if (player === computer) {
-        alert(`Computer (${computer}): draw`);
+        roundResult = "draw";
     } else if (player === "rock") {
         if (computer === "scissors") {
-            alert(`Computer (${computer}): you win`);
+            playerScore++;
+            roundResult = "you win";
         } else {
-            alert(`Computer (${computer}): you lose`);
+            computerScore++;
+            roundResult = "you lose";
         }   
     } else if (player === "paper") {
         if (computer === "rock") {
-            alert(`Computer (${computer}): you win`);
+            playerScore++;
+            roundResult = "you win";
         } else {
-            alert(`Computer (${computer}): you lose`);
+            computerScore++;
+            roundResult = "you lose";
         }   
     }  else if (player === "scissors") {
         if (computer === "paper") {
-            alert(`Computer (${computer}): you win`);
+            playerScore++;
+            roundResult = "you win";
         } else {
-            alert(`Computer (${computer}): you lose`);
-        }   
+            computerScore++;
+            roundResult = "you lose";
+        }  
     }  else {
-        alert("wrong input")
+        alert("wrong input");
+        restartRound = true;
+    }
+    if (!restartRound) {
+        alert(`Computer (${computer}): ${roundResult}
+Player Score:${playerScore}     Computer Score: ${computerScore}`);
+    }
+}
+
+function Game(rounds) {
+    while (gameRound <= rounds) {
+        computerSelection = getComputerChoice();
+        playerSelection = getPlayerChoice(prompt(`Round ${gameRound}: Rock/Paper/Scissors`));
+        playRound(playerSelection,computerSelection);
+        if (restartRound === true) {
+            restartRound = false;
+            continue;
+        }
+        gameRound++;
     }
 }
 
 const CHOICE = ["rock","paper","scissors"];
 let playerSelection;
 let computerSelection;
+let gameRound = 1;
+let playerScore = 0;
+let computerScore = 0;
+let roundResult;
+let restartRound = false;
 
-computerSelection = getComputerChoice();
-playerSelection = getPlayerChoice(prompt("Rock/Paper/Scissors"));
-playRound(playerSelection,computerSelection);
-
+Game(5);
