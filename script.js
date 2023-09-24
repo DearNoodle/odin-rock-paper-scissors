35ed75686de8349072d94eb1d62ff4fc37e43f4b
DearNoodle <wan619395@gmail.com>
2023-09-24 12:09:10 +0800

    show the game Results + wrong prompt inputs

--- script.js
+++ script.js
@@ -6,8 +6,37 @@ function getPlayerChoice(str) {
     return str.toLowerCase();
 }
 
+function playRound(player,computer) {
+    if (player === computer) {
+        alert(`Computer (${computer}): draw`);
+    } else if (player === "rock") {
+        if (computer === "scissors") {
+            alert(`Computer (${computer}): you win`);
+        } else {
+            alert(`Computer (${computer}): you lose`);
+        }   
+    } else if (player === "paper") {
+        if (computer === "rock") {
+            alert(`Computer (${computer}): you win`);
+        } else {
+            alert(`Computer (${computer}): you lose`);
+        }   
+    }  else if (player === "scissors") {
+        if (computer === "paper") {
+            alert(`Computer (${computer}): you win`);
+        } else {
+            alert(`Computer (${computer}): you lose`);
+        }   
+    }  else {
+        alert("wrong input")
+    }
+}
+
 const CHOICE = ["rock","paper","scissors"];
 let playerSelection;
 let computerSelection;
+
 computerSelection = getComputerChoice();
 playerSelection = getPlayerChoice(prompt("Rock/Paper/Scissors"));
+playRound(playerSelection,computerSelection);
+
